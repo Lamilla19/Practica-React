20e7593fc4603a30c92aa37fec6e1eceab81e020
Kevin Lamilla <Kevinlamilla16@gmail.com>
2024-09-29 13:52:52 -0500

    Se creo un archivo de estilos index.css , se creo un archivo de imagenes

---
+++ proyectos/00-hola-mundo/src/App.jsx
@@ -0,0 +1,20 @@
+function App(){
+    return (
+      <article style={{display:"flex" , color : "white"}}>
+        <header>
+            <img src="public/imagenes/Astrobot.jpg" alt="Astrobot"/>
+            <div>
+                <strong>Kevin Lamilla</strong>
+                <span>@klamilla16</span>
+            </div>
+        </header>
+        <aside>
+            <button>
+                seguir
+            </button>
+        </aside>
+      </article>
+    )
+}
+
+export default App;
