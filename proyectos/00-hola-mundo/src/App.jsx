63f538d0826321d5e0404feaf1a306c43833abe6
Kevin Lamilla <Kevinlamilla16@gmail.com>
2024-10-01 23:32:38 -0500

    Se añadio estilos para visualizar los cambios de estados , se añadio los estados y se renderizo una lista utilizando el componente

--- proyectos/00-hola-mundo/src/App.jsx
+++ proyectos/00-hola-mundo/src/App.jsx
@@ -1,14 +1,57 @@
 import "./App.css"
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
+const users = [
+    {
+        userName : "KLamilla16",
+        name : "Kevin Lamilla",
+        isFollowing : true
+    },
+    {
+        userName : "ILamilla18",
+        name : "Isaac Lamilla",
+        isFollowing : false
+    },
+    {
+        userName : "RLamilla22",
+        name : "Anthonny Lamilla",
+        isFollowing : true
+    },
+    {
+        userName : "SLamilla22",
+        name : "Pepe Lamilla",
+        isFollowing : true
+    },   
+    {
+        userName : "MLamilla5",
+        name : "Matias Lamilla",
+        isFollowing : false
+    },
+    {
+        userName : "ELamilla28",
+        name : "Elias Lamilla",
+        isFollowing : true
+    }
+]
 function App(){
-    return (
-    <div className="App">
-        <TwitterFollowCard userName="klamilla16" name="Kevin Lamilla" isFollowing="Seguir"/>
-        <TwitterFollowCard userName="ILamilla18" name="Isaac Lamilla" isFollowing="Seguir"/>
-        <TwitterFollowCard userName="ALamilla22" name="Anthonny Lamilla" isFollowing="Seguir"/>
-    </div>
-)
+    const format = (userName) => `@${userName}`
+ return(
+    <section className="App">
+        {
+            users.map(({userName,name,isFollowing})=>{
+                return(
+                        <TwitterFollowCard 
+                        key={userName}
+                        userName={userName}
+                        name={name}
+                        formatElement={format}
+                        initialIsFollowing={isFollowing}
+                        />  
+                    )
+            })
+        }
+    </section>
+ )
 }
 
 export default App;
--- proyectos/00-hola-mundo/src/TwitterFollowCard.jsx
+++ proyectos/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,4 +1,21 @@
-export function TwitterFollowCard({userName , name , isFollowing}){
+import { useState } from "react";
+
+export function TwitterFollowCard({formatElement , userName , name , initialIsFollowing}){
+
+    //Usamos la desestrucutacion de JavaScript para visualizar mucho mejor el estado .
+    const [isFollowing , setIsFollowing] = useState(initialIsFollowing)
+
+    //Permite validar que cuando sea trua isFollowing el valor en texto sea "Siguiendo" y si es false el valor sea "Seguir"
+    const text = isFollowing ? "Siguiendo" : "Seguir"
+
+    //Nos permitira cambiar los estilos dependiendo el estado del boton 
+    const  buttonisFollowing = isFollowing 
+    ? "tw-followCard-btnSeguir is-following"
+    : "tw-followCard-btnSeguir"
+    //Esta funcion permitira cambiar el estado de true a false o de false a true
+    const handleClick =()=>{
+        setIsFollowing (!isFollowing)
+    }
     return (
         <article className="tw-followCard">
           <header className="tw-followCard-header">
@@ -8,12 +25,13 @@ export function TwitterFollowCard({userName , name , isFollowing}){
               alt="Astrobot"/>
               <div className="tw-followCard-info">
                   <strong>{name}</strong>
-                  <span className="tw-followCard-user">@{userName}</span>
+                  <span className="tw-followCard-user">{formatElement(userName)}</span>
               </div>
           </header>
           <aside>
-              <button className="tw-followCard-btnSeguir">
-                  {isFollowing}
+              <button className={buttonisFollowing} onClick={handleClick}>
+                  <span className="tw-followCard-text">{text}</span>
+                  <span className="tw-followCard-stopFollow">Dejar de seguir</span>
               </button>
           </aside>
         </article>
