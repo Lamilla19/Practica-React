2e3b136042831ce66476d61c5a1052fbd2f4a9a6
Kevin Lamilla <Kevinlamilla16@gmail.com>
2024-10-03 21:55:25 -0500

    Se añadio los estilos de proyecto y se renderizo el tablero

--- proyectos/02-tic-tac-toe/02-tic-tac-toe/src/App.jsx
+++ proyectos/02-tic-tac-toe/02-tic-tac-toe/src/App.jsx
@@ -1,8 +1,42 @@
+/*Creamos una constante para los turnos del juego*/
+/*{} esto denomina que es un objeto*/
+const TURNOS = {
+  X : "x",
+  Y : "y"
+}
+/*Sintaxi*/
+/*const array = Array(longitud).fill(valor);*/
+const tablero = Array(9).fill(null)
+
+const Square = ({children , updateBoard , index})=>{
+  return(
+    <div className="square">
+      {children}
+    </div>
+  )
+}
 
 function App() {
 
   return (
-    <h1> TIC TAC TOE</h1>
+    <main className="board">
+      <h1>TIC TAC TOE</h1>
+      <section className="game">
+        {
+          tablero.map((_,index)=>{
+            return(
+              <Square
+              key={index}
+              index={index}
+              >
+              {index}
+              </Square>            
+            )
+          })
+        }
+      </section>
+
+    </main>
   )
 }
 
